b5737a43f0557ac0bb7e1fd0e816db734e2b8422
Janneman <Jan@kooperativ.net>
2019-10-18 12:35:11 +0200

    flash messages

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,7 +8,11 @@ exports.login = function(req, res) {
       res.redirect('/')
     })
   }).catch(function(e) {
-    res.send(e)
+    req.flash('errors', e)
+    // req.session.flash.errors = [e]
+    req.session.save(function(){
+      res.redirect('/')
+    })
   })
 }
 
@@ -32,6 +36,6 @@ exports.home = function(req, res) {
   if (req.session.user) {
     res.render('home-dashboard', {username: req.session.user.username})
   } else {
-    res.render('home-guest')
+    res.render('home-guest', {errors: req.flash('errors')})
   }
 }
