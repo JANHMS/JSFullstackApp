000b67cb194140d42c0bd9ec33b4c448cd1dedc1
Janneman <Jan@kooperativ.net>
2019-10-18 17:02:46 +0200

    Post in DB

--- router.js
+++ router.js
@@ -3,12 +3,15 @@ const router = express.Router()
 const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
 
+// user related routes
 router.get('/', userController.home)
 router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
-//post related routes
-router.get('/create-post',userController.mustBeLoggedIn, postController.viewCreateScreen)
+// post related routes
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
+router.post('/create-post', userController.mustBeLoggedIn, postController.create)
+router.get('/post/:id', postController.viewSingle)
 
 module.exports = router
