61e296f563a34fd74897ad0e8f481ad92d02e1da
Janneman <Jan@kooperativ.net>
2019-10-18 19:00:04 +0200

    edit view files

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const Post = require('../models/Post')
 
 exports.mustBeLoggedIn = function(req, res, next){
   if (req.session.user) {
@@ -56,4 +57,30 @@ exports.home = function(req, res) {
   } else {
     res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
+}
+
+exports.ifUserExists = function(req, res, next){
+  User.findByUsername(req.params.username).then(function(userDocument){
+    req.profileUser = userDocument
+    next()
+  }).catch(function(){
+    res.render("404")
+  })
+}
+
+exports.profilePostsScreen = function(req,res){
+  //ask post model for post by sertain autthor id
+  Post.findByAuthorId(req.profileUser._id).then(function(posts){
+    res.render('profile',{
+      posts: posts,
+      profileUsername:req.profileUser.username,
+      profileAvatar: req.profileUser.avatar
+    })
+
+  }).catch(function(){
+    res.render('404')
+  })
+
+
+
 }
--- router.js
+++ router.js
@@ -9,9 +9,13 @@ router.post('/register', userController.register)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
 
+router.get('/profile/:username', userController.ifUserExists, userController.profilePostsScreen)
+
+
 // post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
 router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 router.get('/post/:id', postController.viewSingle)
+
 
 module.exports = router
