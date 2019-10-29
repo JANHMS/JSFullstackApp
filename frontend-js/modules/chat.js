62c852176b9feaaea7236603725bf32905fa6135
Janneman <Jan@kooperativ.net>
2019-10-29 14:04:27 +0100

    CSRF Protection

--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -82,7 +82,7 @@ export default class Chat {
     this.chatWrapper.innerHTML = `
     <div class="chat-title-bar">Chat <span class="chat-title-bar-close"><i class="fas fa-times-circle"></i></span></div>
     <div id="chat" class="chat-log"></div>
-
+    
     <form id="chatForm" class="chat-form border-top">
       <input type="text" class="chat-field" id="chatField" placeholder="Type a message…" autocomplete="off">
     </form>
